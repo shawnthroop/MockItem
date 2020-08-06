40c33e8eacc488bb98636eafb729196ed19e456f
Shawn Throop <shawnthroop@gmail.com>
2020-08-06 19:32:21 +0200

    Removed punctuation from generated words, updated ignored files

--- Sources/MockItem/LoremIpsum.swift
+++ Sources/MockItem/LoremIpsum.swift
@@ -3,8 +3,6 @@
 //  Created by Shawn Throop on 06.08.20.
 //
 
-import Foundation
-
 public enum LoremIpsum {
     public static let words = makeWords()
 }
@@ -14,6 +12,16 @@ extension LoremIpsum {
     
     public static func randomWord() -> Substring {
         words.randomElement()!
+    }
+    
+    public static func randomWords(count: Int) -> [Substring] {
+        guard count >= 0 else {
+            return []
+        }
+        
+        return (0..<count).reduce(into: .init()) { names, _ in
+            names.append(randomWord())
+        }
     }
 }
 
@@ -32,6 +40,5 @@ private extension LoremIpsum {
 
     Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
     """
-        .trimmingCharacters(in: .punctuationCharacters)
-        .lowercased()
+        .filter { !$0.isPunctuation && !$0.isNewline }
 }
